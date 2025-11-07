d4cd590cdf1991c1fec9808b223c7c57ffba4b9d
Jean Limonta <jean.limonta06@gmail.com>
2025-11-07 13:36:21 +0100

    fix: use CommonJS for tailwind/postcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
-      height:    { dvh: '100dvh' },
-      minHeight: { dvh: '100dvh' },
+      height: { dvh: "100dvh" },
+      minHeight: { dvh: "100dvh" },
     },
   },
   plugins: [],
